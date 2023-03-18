ca38e17ff9aeb69f05fa535b1042c1ff5f5adcb7
Karen Payne <paynekaren@comcast.net>
2023-03-18 15:37:07 -0700

    new project

---
+++ CommandArgsConsoleSubCommands/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CommandLine;
+using Spectre.Console;
+
+namespace CommandArgsConsoleSubCommands;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "The file to read and display on the console.");
+
+        var foreColorOption = new Option<bool>(
+            name: "--light-mode",
+            description: "Specifies foreground color");
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+
+        var readCommand = new Command("read", "Read and display the file.")
+            {
+                fileOption,
+                foreColorOption
+            };
+        rootCommand.AddCommand(readCommand);
+
+        readCommand.SetHandler(async (file,  lightMode) =>
+        {
+            await ReadFile(file!, lightMode);
+        }, fileOption, foreColorOption);
+
+        return rootCommand.InvokeAsync(args).Result;
+    }
+
+    internal static Task ReadFile(FileInfo file,  bool lightMode)
+    {
+        AnsiConsole.Foreground = lightMode ? Color.Silver : Color.DodgerBlue1;
+        
+        if (File.Exists(file.FullName))
+        {
+            List<string> lines = File.ReadLines(file.FullName).ToList();
+
+            IEnumerable<string[]> chunks = lines.Chunk(10);
+
+            foreach (var chunk in chunks)
+            {
+                AnsiConsole.Write(string.Join("\n", chunk));
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[red]Not found[/] [cyan]{file.FullName}[/]");
+        }
+
+        return Task.CompletedTask;
+
+    }
+}
---
+++ CommandArgsConsoleSubCommands/readme.md
@@ -0,0 +1,17 @@
+﻿# About
+
+This project was taken from a [Microsoft example](https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial) and modified.
+
+> **Note**
+> Microsoft example is good but needed some tweaks.
+
+
+Run from the command line
+
+```
+.\CommandArgsConsoleSubCommands.exe read --file LargePeople.txt --light-mode false
+```
+
+- read is a verb
+- --file is a command to the read verb which expects a file name to read from disk
+- --light-mode specifies the fore color using [Spectre.Console](https://spectreconsole.net/) NuGet package.
